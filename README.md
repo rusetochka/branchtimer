168964a945b6bc1e3746191a134da42a781574be
Olga Shvanskaia <shvanskaia@heinekamp.com>
2023-11-09 19:05:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Branch Timer
+
+Branch Timer is a Visual Studio Code extension that allows you to log branch switches in your Git repositories. Keep track of your work progress by automatically recording the branch name and timestamp whenever you switch branches in your projects.
+
+## Features
+
+- **Manual Logging:** You can log branch switches by clicking CTRL + Shift + 3 with the format "SEAL-XXXX DATE TIME" in a specified log file.
+- **Customizable Log Format:** Customize the log entry format to match your preferences.
+- **Exclude Specific Branches:** Exclude specific branches (e.g., "develop") from being logged.
+
+## Usage
+
+1. **Install the Extension:**
+   - Search for "Branch Timer" in the VS Code Extensions view and install it.
+
+2. **Configure the Log File:**
+   - By default, the extension logs branch switches in a file named `log.txt` within the extension directory. You can customize the log file path in the extension settings.
+
+3. **Switch Branches:**
+   - Whenever you switch branches in your Git repository, call the logging function by clicking CTRL + SHIFT + 3 to log the branch name and timestamp in the specified log file.
+
+4. **View Branch Logs:**
+   - Open the log file using your preferred editor to view the recorded branch switches and timestamps and calculate time spent on each branch.
+
+## Extension Settings
+
+- `branchTimer.logFilePath`: Path to the log file where branch switches will be recorded. Default value: `./log.txt`.
+
+- `branchTimer.logEntryFormat`: Customize the log entry format. Available placeholders: `{prefix}`, `{code}`, `{date}`, `{time}`. Default value: `"{prefix}{code} {date} {time}"`.
+
+- `branchTimer.prefix`: Prefix to be added to the log entry. Default value: `"SEAL-"`.
+
+- `branchTimer.excludedBranches`: Array of branch names to exclude from logging. Default value: `["develop"]`.
+
+- `branchTimer.branchCodeRegex`: Custom regex pattern to match branch codes. Example: `seal-\\d{4}`. Default value: `"seal-\\d{4}"`.
+
+## Requirements
+
+- Visual Studio Code 1.60.0 or higher.
+
+## Known Issues
+
+- None at the moment.
+
+## Release Notes
+
+### 1.0.0
+
+- Initial release of Branch Timer.
---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+import * as vscode from "vscode";
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const logFileName = "log.txt";
+const logFilePath = path.join(__dirname, logFileName);
+
+export function activate(context: vscode.ExtensionContext) {
+  vscode.commands.executeCommand("branch-timer.logBranchSwitch");
+
+  let disposable = vscode.commands.registerCommand(
+    "branch-timer.logBranchSwitch",
+    () => {
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+      if (workspaceFolders && workspaceFolders.length > 0) {
+        const workspaceFolder = workspaceFolders[0];
+        const branchName = execSync("git rev-parse --abbrev-ref HEAD", {
+          cwd: workspaceFolder.uri.fsPath,
+        })
+          .toString()
+          .trim();
+
+        if (branchName.toLowerCase() !== "develop") {
+          const config = vscode.workspace.getConfiguration("branchTimer");
+          const customBranchCodeRegex = config.get<string>(
+            "branchCodeRegex",
+            "seal-\\d{4}"
+          );
+
+          const regex = new RegExp(customBranchCodeRegex, "i");
+
+          if (regex.test(branchName)) {
+            const config = vscode.workspace.getConfiguration("branchTimer");
+            const prefix = config.get<string>('prefix', '');
+
+            const sealCodeMatch = branchName.match(/\d{4}/);
+            const code = sealCodeMatch ? sealCodeMatch[0] : branchName;
+            const logEntry = `${prefix}${code.toUpperCase()} ${getFormattedDate()}`;
+            // Handle the logEntry by appending it to a log file
+            fs.appendFileSync(logFilePath, `${logEntry}\n`);
+
+            vscode.window.showInformationMessage(
+              `Logged branch switch: ${logEntry}`
+            );
+          }
+        }
+      } else {
+        vscode.window.showErrorMessage("No workspace folder found.");
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function getFormattedDate() {
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
+
+export function deactivate() {}
